57be968160da2ea78ae15be3e7b30dc0bab80138
Wesley Selmer <wesley.selmer@gmail.com>
2023-09-25 09:09:49 -0300

    Projeto finalizado

--- src/Contador.java
+++ src/Contador.java
@@ -12,15 +12,23 @@ public class Contador {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch ( exception) {
+		}catch ( ParametrosInvalidosException e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		
 		
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
 	}
 }
